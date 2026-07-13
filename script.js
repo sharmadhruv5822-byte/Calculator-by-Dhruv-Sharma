2953047e5ef2cbdf965ab2a789fda9db58da56f4
Your Name <yourname@example.com>
2026-07-13 12:36:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+// Keyboard Support
+document.addEventListener("keydown", (event) => {
+
+    const key = event.key;
+
+    if (
+        (key >= "0" && key <= "9") ||
+        key === "+" ||
+        key === "-" ||
+        key === "*" ||
+        key === "/" ||
+        key === "."
+    ) {
+        appendValue(key);
+    }
+
+    if (key === "Enter") {
+        calculate();
+    }
+
+    if (key === "Backspace") {
+        deleteLast();
+    }
+
+    if (key === "Escape") {
+        clearDisplay();
+    }
+});
---
+++ scriptt.js
@@ -0,0 +1,15 @@
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach((entry)=>{
+
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+
+});
+});
+
+hiddenElements.forEach((el)=>{
+observer.observe(el);
+});
